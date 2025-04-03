1a89276b9250e593369aeeff35431c2b947965f3
Akhileshyadu00 <94223259+Akhileshyadu00@users.noreply.github.com>
2025-04-03 05:52:10 +0000

    update

--- linklistcycle_141.js
+++ linklistcycle_141.js
@@ -29,4 +29,21 @@ var hasCycle = function(head) {
 Time Complexity: O(n) → Each node is visited at most once.
 
 Space Complexity: O(1) → Uses only two pointers, No extra space is used.
+
+
+
+Time Complexity: O(n)
+
+The slow pointer (slow) moves one step at a time.
+The fast pointer (fast) moves two steps at a time.
+In the worst case (if there's a cycle), fast catches slow in at most n iterations.
+If there’s no cycle, fast will reach null in O(n).
+Since each node is visited at most once, the total time complexity is O(n).
+
+Space Complexity: O(1)
+
+The solution uses only two pointers (slow and fast).
+No extra data structures (like arrays, hash sets, or recursion) are used.
+Therefore, the space complexity is O(1).
+
 */
